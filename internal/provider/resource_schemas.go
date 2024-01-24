0b1e81bb101f78e3dfda76d5bf95389514b49d49
Kit Ewbank <Kit_Ewbank@hotmail.com>
2024-01-24 15:23:19 -0500

    Run 'make resource-schemas'.

--- internal/provider/resource_schemas.go
+++ internal/provider/resource_schemas.go
@@ -1,7 +1,7 @@
 // Copyright (c) HashiCorp, Inc.
 // SPDX-License-Identifier: MPL-2.0
 
-//go:generate go run generators/tfplugingen/main.go -resource awscc_logs_log_group -cfschema ../service/cloudformation/schemas/AWS_Logs_LogGroup.json -- ../aws/xray/log_group_resource_spec.json
-//go:generate tfplugingen-framework generate resources --input ../aws/xray/log_group_resource_spec.json --output ../aws/logs --package logs
+//go:generate go run generators/tfplugingen/main.go -resource awscc_logs_log_group -cfschema ../service/cloudformation/schemas/AWS_Logs_LogGroup.json -- ../aws/logs/log_group_resource_spec.json
+//go:generate tfplugingen-framework generate resources --input ../aws/logs/log_group_resource_spec.json --output ../aws/logs --package logs
 
 package provider
